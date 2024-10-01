c802ce6e1b3fac90fc45514a9062f253b447ad88
Jiayuan Li <1765160560@qq.com>
2024-10-01 19:07:38 +0800

    finish string challenges

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,24 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn trim_me(input: &str) -> String {
-    // TODO: Remove whitespace from both ends of a string!
-    ???
+    String::from(input.trim())
 }
 
+// to_string方法，可以用于任何实现了Display trait的类型
+// 字符串字面量也实现了它
 fn compose_me(input: &str) -> String {
-    // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+//     let mut s = input.to_string();
+//     s.push_str(" world!");
+//     s
+
+    // format!("{} world!", input)
+
+    input.to_string() + " world!"
 }
 
 fn replace_me(input: &str) -> String {
-    // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    input.replace("cars", "balloons")
 }
 
 #[cfg(test)]
