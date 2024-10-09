908af2f4e69536a351c21c8c57ab59906e100c45
Jiayuan Li <1765160560@qq.com>
2024-10-09 11:09:15 +0800

    finish tests1-4

--- exercises/tests/tests3.rs
+++ exercises/tests/tests3.rs
@@ -4,10 +4,10 @@
 // the test passes. Then write a second test that tests whether we get the
 // result we expect to get when we call `is_even(5)`.
 //
-// Execute `rustlings hint tests3` or use the `hint` watch subcommand for a
+// Execute `rustlings hint t ests3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 pub fn is_even(num: i32) -> bool {
     num % 2 == 0
@@ -19,11 +19,14 @@ mod tests {
 
     #[test]
     fn is_true_when_even() {
-        assert!();
+        assert!(is_even(4));
+        assert!(is_even(6));
+        assert!(is_even(10000000));
+        assert!(is_even(0));
     }
 
     #[test]
     fn is_false_when_odd() {
-        assert!();
+        assert!(!is_even(5));
     }
 }
