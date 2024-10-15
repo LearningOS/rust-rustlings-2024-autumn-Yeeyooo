d9d1cf9735955e5431ab0ba5f4853d61ab0fab5e
Jiayuan Li <1765160560@qq.com>
2024-10-15 09:35:12 +0800

    finish macros challenges

--- exercises/macros/macros3.rs
+++ exercises/macros/macros3.rs
@@ -5,9 +5,10 @@
 // Execute `rustlings hint macros3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
+// #[macro_export]标注表示，只要将定义了宏的crate引入作用域
+// 宏就应当是可用的。如果没有该标注，这个宏就不能被引入作用域
 mod macros {
+    #[macro_export]
     macro_rules! my_macro {
         () => {
             println!("Check out my macro!");
